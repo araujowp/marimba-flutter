64b9e0dfebd906047d4654790fce8bf6e9f5ebe3
Wagner.Araujo <araujowp@gmail>
2021-04-06 08:07:21 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MarimbaApp());
 
 class MarimbaApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              child: Text('tocar?'),
+              onPressed: (){
+                print('tocando');
+                final player  = AudioCache();
+                player.play('nota1.wav');
+              },
+            ),
+          ),
         ),
       ),
     );
