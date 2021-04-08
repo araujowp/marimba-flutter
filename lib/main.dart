f9d1f75bda10f253597a77f21132cfbd6177ccab
Wagner.Araujo <araujowp@gmail>
2021-04-08 19:29:32 -0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MarimbaApp());
 
 class MarimbaApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: Text('tocar?'),
-              onPressed: (){
-                print('tocando');
-                final player  = AudioCache();
-                player.play('nota1.wav');
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              botao(nota: 1, cor: Colors.lightGreenAccent),
+              botao(nota: 2, cor: Colors.lightBlueAccent),
+              botao(nota: 3, cor: Colors.yellowAccent),
+              botao(nota: 4, cor: Colors.red),
+              botao(nota: 5, cor: Colors.grey),
+              botao(nota: 6, cor: Colors.purple),
+              botao(nota: 7, cor: Colors.brown),
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget botao({int nota, Color cor}) {
+  return Expanded(
+    child: TextButton(
+      style: TextButton.styleFrom(backgroundColor: cor),
+      child: null,
+      onPressed: () {
+        print('tocando');
+        final player = AudioCache();
+        player.play('nota$nota.wav');
+      },
+    ),
+  );
 }
